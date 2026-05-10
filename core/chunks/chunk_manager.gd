991dedc87c1c43d5da493e3cc77914e1fcfc8853
Alexander Stevens <alex.stvn2005@gmail.com>
2026-05-10 16:34:56 -0700

    feat: added multithreaded terrain generation that uses all but one availible thread on users system so as to not block main

--- core/chunks/chunk_manager.gd
+++ core/chunks/chunk_manager.gd
@@ -4,22 +4,42 @@ extends Node
 
 const CHUNK_SIZE := Vector3i(32, 16, 32)
 
+var NUM_GENERATOR_THREADS := OS.get_processor_count() - 1
 var perlin_noise_generator := FastNoiseLite.new()
 
 func _ready():
-	_generate_chunks(3, 3)
-
-func _generate_chunks(num_rows: int, num_cols: int):
-	var chunk_pos := Vector3i(0, 0, 0)
 	perlin_noise_generator.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	perlin_noise_generator.seed = randi()
+	var chunk_positions: Array[Vector3i] = []
+	var num_rows := 16
+	var num_cols := 16
+	var chunk_pos := Vector3i(0, 0, 0)
 
 	for row_num in range(num_rows):
 		chunk_pos.z = 0
 		for col_num in range(num_cols):
-			var chunk: StaticBody3D = chunk_scene.instantiate()
-			chunk.create_chunk(chunk_pos, perlin_noise_generator)
-			add_child(chunk)
-
+			chunk_positions.append(chunk_pos)
 			chunk_pos.z += CHUNK_SIZE.z
 		chunk_pos.x += CHUNK_SIZE.x
+
+	@warning_ignore("integer_division")
+	var num_chunks_per_thread := chunk_positions.size() / NUM_GENERATOR_THREADS
+	var chunk_positions_index := 0
+
+	for thread_num in range(NUM_GENERATOR_THREADS):
+		var slice_size := num_chunks_per_thread
+
+		if thread_num == NUM_GENERATOR_THREADS - 1:
+			Thread.new().start(_generate_chunks.bind(chunk_positions.slice(chunk_positions_index, chunk_positions_index + num_chunks_per_thread + chunk_positions.size() % NUM_GENERATOR_THREADS)))
+			slice_size += chunk_positions.size() % NUM_GENERATOR_THREADS
+		else:
+			Thread.new().start(_generate_chunks.bind(chunk_positions.slice(chunk_positions_index, chunk_positions_index + num_chunks_per_thread)))
+		
+		chunk_positions_index += slice_size
+
+
+func _generate_chunks(chunk_positions: Array[Vector3i]):
+	for chunk_pos in chunk_positions:
+		var chunk: StaticBody3D = chunk_scene.instantiate()
+		chunk.create_chunk(chunk_pos, perlin_noise_generator)
+		call_deferred("add_child", chunk)
